03173d5bbd0dc994355f232f0ef5cae48c062db0
manhhungtmh <48795266+manhhungtmh@users.noreply.github.com>
2021-02-26 03:04:15 +0700

    bai tap SPA

---
+++ BaiTapSPA.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $.get("https://6037e3394e3a9b0017e927ed.mockapi.io/api/v1/usol", function (data, status) {
+        if (status == "success") {
+            $("#Introduce").html(data[0]);
+            $("#Address").html(data[1]);
+            $("#Contact").html(data[2]);
+        } else {
+            $(".container").html("<h1>WEBSITE ĐANG BẢO TRÌ, VUI LÒNG TRUY CẬP LẠI SAU !!!</h1>");
+        }
+    });
+    hideAllData();
+    $(".btnShow").click(function () {
+        var DataShow = $(this).attr("id");
+        if (DataShow == "showIntroduce") {
+            hideAllData();
+            $("#Introduce").show();
+        } else if (DataShow == "showAddress") {
+            hideAllData();
+            $("#Address").show();
+        } else if (DataShow == "showContact") {
+            hideAllData();
+            $("#Contact").show();
+        }
+    });
+    function hideAllData() {
+        $("#Introduce").hide();
+        $("#Address").hide();
+        $("#Contact").hide();
+    }
+});
